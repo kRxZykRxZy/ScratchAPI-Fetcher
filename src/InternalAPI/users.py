d540d16b9332b4374cc2574ce3c0c4c3a8bb8271
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-14 16:55:16 +0100

    Update users.py

--- src/InternalAPI/users.py
+++ src/InternalAPI/users.py
@@ -1,14 +1,6 @@
 from flask import jsonify
 import requests
-import importlib.util
-import sys
-import os
-
-module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'API Helper', 'limiter.py'))
-spec = importlib.util.spec_from_file_location("limiter", module_path)
-limiter = importlib.util.module_from_spec(spec)
-sys.modules["limiter"] = limiter
-spec.loader.exec_module(limiter)
+from API_Helper.limiter import limiter
 
 @limiter.limit("10 per minute")
 def userProfile(username):
