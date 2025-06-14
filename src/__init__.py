e36e72a192f5539dac0445d24c8c6b44412d2c8b
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-14 17:02:19 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,8 @@
 from InternalAPI.users import userProfile
 from flask import *
+from InternalAPI.API_Helper.limiter import limiter
+
 app = Flask(__name__)
+limiter.init_app(app)
 
 app.add_url_rule("/users/<path:subpath>", methods=['GET', 'POST', 'OPTIONS'], view_func=userProfile)
